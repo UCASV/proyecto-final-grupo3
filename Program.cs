8e3fdab482e1abbc182442c0b866d3c6439500c6
bleczz <00400519@uca.edu.sv>
2021-06-26 19:44:39 -0600

    Follow up form DB Connection

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace ProyectoVacunacionCovid
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Proyecto.RegistroCita());
+            Application.Run(new AppointmentFollowUpSE());
         }
     }
 }
